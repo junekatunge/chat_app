81933a4d1665bedc3eb8fcf06ac16bfc85b2b771
junekatunge <junekatunge@JuneMacBook.local>
2022-07-26 14:29:41 +0300

    all changes

---
+++ chatting/angular-chat/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RouterModule } from '@angular/router'; // for routing
+import { HttpClientModule } from '@angular/common/http'; // for the http client
+import { FormsModule } from '@angular/forms'; // to handle forms
+
+import { AppComponent } from './app.component';
+import { JoinComponent } from './join/join.component';
+import { ChatComponent } from './chat/chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    JoinComponent,
+    ChatComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'join', component: JoinComponent },
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
